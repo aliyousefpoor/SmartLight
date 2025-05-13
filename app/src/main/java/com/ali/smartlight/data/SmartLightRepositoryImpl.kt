c0b42b1954f812b5cc3586225f7f642e09bc6698
AliYousefpoor <aliyousefpoor47@gmail.com>
2025-05-13 18:42:09 +0330

    change repository implementation

--- app/src/main/java/com/ali/smartlight/data/SmartLightRepositoryImpl.kt
+++ app/src/main/java/com/ali/smartlight/data/SmartLightRepositoryImpl.kt
@@ -1,28 +1,18 @@
 package com.ali.smartlight.data
 
 import com.ali.smartlight.data.mqtt.MqttClientManager
-import com.ali.smartlight.domain.model.LightState
 import com.ali.smartlight.domain.repository.SmartLightRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 class SmartLightRepositoryImpl @Inject constructor(
-    private val mqttClientManager: MqttClientManager
+    private val mqttClientManager: MqttClientManager,
 ) : SmartLightRepository {
+    override val isConnected = mqttClientManager.isConnected
+    override val lightState = mqttClientManager.lightState
+    override val brightness = mqttClientManager.brightness
 
-    override fun toggleLight(isOn: Boolean) {
-        mqttClientManager.publishState(isOn)
-    }
-
-    override fun setBrightness(brightness: Int) {
-        mqttClientManager.publishBrightness(brightness)
-    }
-
-    override fun observeLightState(): Flow<LightState> = combine(
-        mqttClientManager.lightState,
-        mqttClientManager.brightness
-    ) { isOn, brightness ->
-        LightState(isOn, brightness)
-    }
+    override fun connect() = mqttClientManager.connect()
+    override fun publishState(isOn: Boolean) = mqttClientManager.publishState(isOn)
+    override fun publishBrightness(value: Int) = mqttClientManager.publishBrightness(value)
+    override fun disconnect() = mqttClientManager.disconnect()
 }
